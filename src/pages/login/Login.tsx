a65955830b2862569b72002a3c8a2509040f20b5
eddyzera <edgar.nascimento@newgo.com.br>
2022-02-03 12:34:27 -0300

    [feat]: page Login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import LogoTipo from '../../assets/svg/logo-tipo.svg'
 export const Login: React.FC = () => {
   return (
-    <div className="login">
-      <h1>Hello Guys</h1>
-    </div>
+    <>
+      <header className="header">
+        <div className="header__logo">
+          <img src={LogoTipo} alt="iRemember" />
+          <h1>iRemember</h1>
+        </div>
+        <div className="header__button">
+          <button className="login">entrar</button>
+          <button className="register">cadastrar</button>
+        </div>
+      </header>
+      <div className="login">
+        <div className="login__container">
+          <div className="login-content">
+            <h2>Domine o seu tempo de uma maneira mais facil.</h2>
+            <p>
+              Estamos aqui para te ajudar a lembrar de suas tarefas do dia a
+              dia, uma forma simples sem complicações.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
