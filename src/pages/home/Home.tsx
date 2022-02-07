caf736e429eb732de2e1951d31d225eb193c0b37
eddyzera <edgar.nascimento@newgo.com.br>
2022-02-07 19:10:16 -0300

    [feat]: creation page Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../../components/header'
 
 export const Home: React.FC = () => {
   return (
-    <div className="home">
-      <h1>Home</h1>
-    </div>
+    <>
+      <Header isLogged={true} />
+      <div className="home">
+        <h1>Home</h1>
+      </div>
+    </>
   )
 }
