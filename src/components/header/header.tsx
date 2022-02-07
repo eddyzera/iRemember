2198c446641e884dc356a3ee10562b31923c57b7
eddyzera <edgar.nascimento@newgo.com.br>
2022-02-07 18:55:13 -0300

    [refactor]: Component Header with prop isLogged

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import LogoTipo from '../../assets/svg/logo-tipo.svg'
+import { HeaderProps } from './types'
 
-export const Header: React.FC = () => {
+export const Header: React.FC<HeaderProps> = ({
+  isLogged = false
+}: HeaderProps) => {
   return (
     <header className="header">
       <div className="header__logo">
-        <img src={LogoTipo} alt="iRemember" />
-        <h1>iRemember</h1>
-      </div>
-      <div className="header__button">
-        <Link to="/enter">
-          <a className="login">entrar</a>
-        </Link>
-        <Link to="/register">
-          <a className="register">cadastrar</a>
+        <Link to="/">
+          <img src={LogoTipo} alt="iRemember" />
+          <h1>iRemember</h1>
         </Link>
       </div>
+      {isLogged ? (
+        <div>Logado</div>
+      ) : (
+        <div className="header__button">
+          <Link to="/enter">
+            <a className="login">entrar</a>
+          </Link>
+          <Link to="/register">
+            <a className="register">cadastrar</a>
+          </Link>
+        </div>
+      )}
     </header>
   )
 }
