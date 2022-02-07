e7f788e74dbedbf3d76bcffdfc62b67aa50a001e
eddyzera <edgar.nascimento@newgo.com.br>
2022-02-07 17:10:04 -0300

    [style]: creation style for page Enter

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
-
+import { MdAlternateEmail, MdVpnKey } from 'react-icons/md'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { ILoginForm } from './types'
 export const LoginForm: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<ILoginForm>()
+  const onSubmit: SubmitHandler<ILoginForm> = (data) => {
+    console.log('data', data)
+  }
   return (
     <div className="login-form">
-      <form action="">
-        <h1>Ola mundo</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="register-input-group">
+          <MdAlternateEmail color="#8a939b" />
+          <input
+            type="email"
+            placeholder="Digite o seu e-mail..."
+            {...register('email', { required: true })}
+          />
+        </div>
+        <div className="register-input-group">
+          <MdVpnKey color="#8a939b" />
+          <input
+            type="password"
+            placeholder="Digite o seu password..."
+            {...register('password', { required: true })}
+          />
+        </div>
       </form>
     </div>
   )
