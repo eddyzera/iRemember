74ef1b8116a88e0acf92f175134ad9ce92188bcc
eddyzera <edgar.nascimento@newgo.com.br>
2022-02-09 14:58:13 -0300

    [refactor]: Header with structure  diferent when logged

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -5,26 +5,15 @@ import { FcCalendar, FcApproval, FcCancel } from 'react-icons/fc'
 export const Aside: React.FC = () => {
   return (
     <aside className="aside">
-      <Link to="/perfil">
-        <a className="perfil">
-          <div className="perfil__container">
-            <img
-              src="https://res.cloudinary.com/practicaldev/image/fetch/s--M7lfo-Mf--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/335826/d074e5ab-472e-417d-a9dd-bcec13063919.jpg"
-              alt=""
-            />
-            <p>Edgar Silva</p>
-          </div>
-        </a>
-      </Link>
       <div className="aside-categories">
         <button className="aside__filter-buttons">
-          <FcCalendar /> filtra por 5 dias
+          <FcCalendar /> filtrar por 5 dias
         </button>
         <button className="aside__filter-buttons">
-          <FcCalendar /> filtra por 15 dias
+          <FcCalendar /> filtrar por 15 dias
         </button>
         <button className="aside__filter-buttons">
-          <FcCalendar /> filtra por 30 dias
+          <FcCalendar /> filtrar por 30 dias
         </button>
         <button className="aside__filter-buttons">
           <FcApproval /> notas arquivadas
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { MdNotificationsNone } from 'react-icons/md'
 import LogoTipo from '../../assets/svg/logo-tipo.svg'
 import { HeaderProps } from './types'
 
@@ -15,7 +16,22 @@ export const Header: React.FC<HeaderProps> = ({
         </Link>
       </div>
       {isLogged ? (
-        <div>Logado</div>
+        <div className="header-logged">
+          <button className="header-logged-create">criar nota</button>
+          <Link to="/notification">
+            <a className="notification">
+              <div className="notification__container">
+                <MdNotificationsNone color="#8a939b" />
+              </div>
+            </a>
+          </Link>
+          <button className="header-logged__perfil">
+            <img
+              src="https://res.cloudinary.com/practicaldev/image/fetch/s--M7lfo-Mf--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/335826/d074e5ab-472e-417d-a9dd-bcec13063919.jpg"
+              alt=""
+            />
+          </button>
+        </div>
       ) : (
         <div className="header__button">
           <Link to="/enter">
